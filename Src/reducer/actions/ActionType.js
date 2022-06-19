c741fe5331012d0514c7afb0aa17ecedf418a820
DharmendraSinghMoreYeahs <88182630+DharmendraSinghMoreYeahs@users.noreply.github.com>
2022-06-19 09:56:13 +0530

    Update ActionType.js

--- Src/reducer/actions/ActionType.js
+++ Src/reducer/actions/ActionType.js
@@ -1,8 +1,2 @@
-export const SET_MSG_LIST = 'SET_MSG_LIST';
-export const SET_SETTING_DATA = 'SET_SETTING_DATA';
-export const SET_PHONE_INFO = 'SET_PHONE_INFO';
-export const SENT_MSG_ON_OFF = 'SENT_MSG_ON_OFF';
-export const SET_USER_INFO_TOKEN = 'SET_USER_INFO_TOKEN';
-
 export const SET_All_NEWS_LIST = 'SET_All_NEWS_LIST';
 export const SET_NEWS_BY_ID = 'SET_NEWS_BY_ID';
